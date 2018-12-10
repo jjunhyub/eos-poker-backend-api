7e4008f187c771ce0b154b04738150e76720dcec
임문혁 <godmoon00@naver.com>
2018-12-10 21:57:15 +0900

    진행중인 게임도 가져오기

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -8,6 +8,6 @@ use App\Game;
 class GameController extends Controller
 {
   public function getGameLogs(){
-    return Game::where('winner_name', '!=', '')->orderBy('id', 'desc')->take(24)->get();
+    return Game::where('winner_name', '!=', '')->orWhere('is_gaming', true)->orderBy('id', 'desc')->take(24)->get();
   }
 }
